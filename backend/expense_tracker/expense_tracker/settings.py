082bf9076ffb2d9dc2a9d15319da572504d301eb
Navin Karthikeyan <navinkarthikeyan12@gmail.com>
2024-08-29 16:07:40 +0530

    smtp update

--- backend/expense_tracker/expense_tracker/settings.py
+++ backend/expense_tracker/expense_tracker/settings.py
@@ -146,4 +146,9 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:5173',  # React frontend
 ]
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'expensetrackerganit@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = 'sduw alid sedf vdph'
